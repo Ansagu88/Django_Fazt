86c8ac18e0096bfeaccd3e2bfc7113f946618ab4
Oscar Andres Sanchez Gutierrez <105839270+Ansagu88@users.noreply.github.com>
2022-10-21 23:26:33 -0300

    a Mitad de camino

--- task/views.py
+++ task/views.py
@@ -1,10 +1,11 @@
 
-from django.shortcuts import render, HttpResponse, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import get_object_or_404, render, HttpResponse, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-
+from .forms import TaskForm
+from .models import Task
 # Create your views here.
 
 
@@ -36,19 +37,48 @@ def signup(request):
             })
 
 def tasks(request):
-    return render(request, 'tasks.html')
+    tasks = Task.objects.filter(user=request.user, completed__isnull=True)
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+            'form': TaskForm()
+            })
+
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {
+                'form': TaskForm(),
+                'error': 'Bad data passed in. Try again.'
+                })
+
+def task_detail(request, task_id):
+        task= get_object_or_404(Task, id=task_id, user=request.user)
+        return render(request, 'tasks_detail.html', { 'task': task })
+    
+
 
 def signout(request):
     logout(request)
     return redirect('home')
 
-def loginuser(request):
+def signin(request):
     if request.method == 'GET':
-        return render(request, 'login.html')
+        return render(request, 'signin.html', {'form': AuthenticationForm()})
     else:
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
-            return render(request, 'login.html', {'error': 'Username and password did not match'})
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm(),
+                'error': 'Username and password did not match'
+                })
         else:
             login(request, user)
             return redirect('tasks')
