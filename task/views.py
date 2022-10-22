d55d373fea724fa066c662b22449e08e1a5d9c4e
Oscar Andres Sanchez Gutierrez <105839270+Ansagu88@users.noreply.github.com>
2022-10-22 16:56:30 -0300

    finalizando proyecto

--- task/views.py
+++ task/views.py
@@ -6,12 +6,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html')
-
 
 def signup(request):
 
@@ -36,10 +37,17 @@ def signup(request):
             'error':'Password did not match'
             })
 
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, completed__isnull=True)
     return render(request, 'tasks.html', {'tasks': tasks})
 
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, completed__isnull=False).order_by('-completed')
+    return render(request, 'tasks.html', {'tasks': tasks})
+ 
+@login_required
 def create_task(request):
     if request.method == 'GET':
         return render(request, 'create_task.html', {
@@ -59,12 +67,41 @@ def create_task(request):
                 'error': 'Bad data passed in. Try again.'
                 })
 
+@login_required
 def task_detail(request, task_id):
-        task= get_object_or_404(Task, id=task_id, user=request.user)
-        return render(request, 'tasks_detail.html', { 'task': task })
-    
+        if request.method == 'GET':
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(instance=task)
+            return render(request, 'tasks_detail.html', {'task': task, 'form': form})
+        else:
+            try:
+                task = get_object_or_404(Task, pk=task_id, user=request.user)
+                form = TaskForm(request.POST, instance=task)
+                form.save()
+                return redirect('tasks')
+            except ValueError:
+                return render(request, 'tasks_detail.html', {
+                    'task': task,
+                    'form': form,
+                    'error': 'Bad info'
+                    })
 
+@login_required
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.completed = timezone.now()
+        task.save()
+        return redirect('tasks')
 
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
+
+@login_required
 def signout(request):
     logout(request)
     return redirect('home')
